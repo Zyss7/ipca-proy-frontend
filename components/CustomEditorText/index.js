3f7a9d17930d904be859bc2d028a6aa274fd06d8
Zyss7 <sebastianv524@gmail.com>
2021-02-27 00:53:39 -0500

    REFACTOR: se arreglo problema con el lang de suneditor

--- components/CustomEditorText/index.js
+++ components/CustomEditorText/index.js
@@ -1,9 +1,9 @@
-import { ErrorMessage } from "@hookform/error-message";
-import React from "react";
-import { Form } from "react-bootstrap";
-import { Controller, useFormContext } from "react-hook-form";
-import SunEditor from "suneditor-react";
-import lang from "suneditor/src/lang";
+import { ErrorMessage } from '@hookform/error-message';
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import { Controller, useFormContext } from 'react-hook-form';
+import SunEditor from 'suneditor-react';
+import es from 'suneditor/src/lang/es';
 
 const CustomEditorText = ({ label, name, rules }) => {
   const { control, errors } = useFormContext();
@@ -15,91 +15,91 @@ const CustomEditorText = ({ label, name, rules }) => {
         name={name}
         control={control}
         rules={rules}
-        defaultValue=''
+        defaultValue=""
         render={({ onChange, value }) => (
           <SunEditor
             setContents={value}
             onChange={onChange}
             height={300}
             setOptions={{
-              mode: "classic",
-              katex: "window.katex",
+              mode: 'classic',
+              katex: 'window.katex',
               charCounter: true,
-              charCounterType: "char",
+              charCounterType: 'char',
               imageGalleryUrl:
-                "https://etyswjpn79.execute-api.ap-northeast-1.amazonaws.com/suneditor-demo",
+                'https://etyswjpn79.execute-api.ap-northeast-1.amazonaws.com/suneditor-demo',
               videoFileInput: false,
-              youtubeQuery: "",
-              tableCellControllerPosition: "",
+              youtubeQuery: '',
+              tableCellControllerPosition: '',
               tabDisable: true,
               shortcutsDisable: [
-                "bold",
-                "strike",
-                "underline",
-                "italic",
-                "undo",
-                "indent",
+                'bold',
+                'strike',
+                'underline',
+                'italic',
+                'undo',
+                'indent',
               ],
               templates: [
                 {
-                  name: "Template-1",
-                  html: "<p>HTML source1</p>",
+                  name: 'Template-1',
+                  html: '<p>HTML source1</p>',
                 },
                 {
-                  name: "Template-2",
+                  name: 'Template-2',
                   html: '<p style="font-weight: 600;">HTML source2</p>',
                 },
               ],
               buttonList: [
                 [
-                  "undo",
-                  "redo",
-                  "font",
-                  "fontSize",
-                  "formatBlock",
-                  "paragraphStyle",
-                  "blockquote",
-                  "bold",
-                  "underline",
-                  "italic",
-                  "strike",
-                  "subscript",
-                  "superscript",
-                  "fontColor",
-                  "hiliteColor",
-                  "textStyle",
-                  "removeFormat",
-                  "outdent",
-                  "indent",
-                  "align",
-                  "horizontalRule",
-                  "list",
-                  "lineHeight",
-                  "table",
-                  "link",
-                  "image",
-                  "video",
-                  "audio",
-                  "math",
-                  "imageGallery",
-                  "fullScreen",
-                  "showBlocks",
-                  "codeView",
-                  "preview",
-                  "print",
-                  "save",
-                  "template",
+                  'undo',
+                  'redo',
+                  'font',
+                  'fontSize',
+                  'formatBlock',
+                  'paragraphStyle',
+                  'blockquote',
+                  'bold',
+                  'underline',
+                  'italic',
+                  'strike',
+                  'subscript',
+                  'superscript',
+                  'fontColor',
+                  'hiliteColor',
+                  'textStyle',
+                  'removeFormat',
+                  'outdent',
+                  'indent',
+                  'align',
+                  'horizontalRule',
+                  'list',
+                  'lineHeight',
+                  'table',
+                  'link',
+                  'image',
+                  'video',
+                  'audio',
+                  'math',
+                  'imageGallery',
+                  'fullScreen',
+                  'showBlocks',
+                  'codeView',
+                  'preview',
+                  'print',
+                  'save',
+                  'template',
                 ],
               ],
               lang: lang.es,
-              "lang(In nodejs)": "es",
+              'lang(In nodejs)': 'es',
             }}
           />
         )}
       />
 
       <ErrorMessage errors={errors} name={name}>
-        {({ message }) => <p className='text-danger'>{message}</p>}
+        {({ message }) => <p className="text-danger">{message}</p>}
       </ErrorMessage>
     </Form.Group>
   );
